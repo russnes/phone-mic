263f3a17a0383968d423c250f334b64ac493fc11
Simen <simen.s.russnes@gmail.com>
2018-09-09 11:30:51 +0200

    added skin

--- core/src/com/simernes/phonemic/PhoneMic.java
+++ core/src/com/simernes/phonemic/PhoneMic.java
@@ -1,26 +1,17 @@
 package com.simernes.phonemic;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.audio.AudioRecorder;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 
-public class PhoneMic extends ApplicationAdapter {
+public class PhoneMic extends Game {
 
 	
 	@Override
 	public void create () {
-		MainScreen mainScreen = new MainScreen(new Skin());
-		Gdx.graphics.setS
-	}
-
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		Skin skin = new Skin(Gdx.files.internal("skins/default/uiskin.json"));
+		MainScreen mainScreen = new MainScreen(skin);
+		setScreen(mainScreen);
 	}
 	
 	@Override
